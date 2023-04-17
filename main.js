6b916d3b5f373dd92dc343e123e756dee6cd05eb
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-17 19:18:21 +0100

    FEAT: Sucessfully showing both cameras now

--- main.js
+++ main.js
@@ -25,21 +25,48 @@ let init = async () => {
     await channel.join()
     channel.on('MemberJoined', handleUserJoined)
 
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
     localStream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true })
     document.getElementById('user-1').srcObject = localStream
 
-    createOffer()
+
+}
+
+let handleMessageFromPeer = async (message, memberId) => {
+    message = JSON.parse(message.text)
+
+    if (message.type === 'offer') {
+        createAnswer(memberId, message.offer)
+    }
+
+    if (message.type === 'answer') {
+        addAnswer(message.answer)
+    }
+
+    if (message.type === 'candidate') {
+        if (peerConnection) {
+            peerConnection.addIceCandidate(message.candidate)
+        }
+    }
+
 }
 
 let handleUserJoined = async (memberId) => {
-    console.log(`User $memberId} has joined the channel`);
+    console.log(`User ${memberId} has joined the channel`);
+    createOffer(memberId)
 }
 
-let createOffer = async () => {
+let createPeerConnection = async (memberId) => {
     peerConnection = new RTCPeerConnection(servers)
 
     remoteStream = new MediaStream()
     document.getElementById('user-2').srcObject = remoteStream
+
+    if (!localStream) {
+        localStream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+        document.getElementById('user-1').srcObject = localStream
+    }
 
     localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream)
@@ -53,13 +80,44 @@ let createOffer = async () => {
 
     peerConnection.onicecandidate = async (event) => {
         if (event.candidate) {
-            // console.log('New ICE Candifate: ', event.candidate);
+            client.sendMessageToPeer({
+                text: JSON.stringify({
+                    'type': "candidate", 'candidate': event.candidate
+                })
+            }, memberId)
         }
     }
+}
+
+let createOffer = async (memberId) => {
+    await createPeerConnection(memberId)
 
     let offer = await peerConnection.createOffer()
     await peerConnection.setLocalDescription(offer)
-    console.log('offer', offer);
+
+    client.sendMessageToPeer({
+        text: JSON.stringify({
+            'type': "offer", 'offer': offer
+        })
+    }, memberId)
+}
+
+let createAnswer = async (memberId, offer) => {
+    await createPeerConnection(memberId)
+    await peerConnection.setRemoteDescription(offer)
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+    client.sendMessageToPeer({
+        text: JSON.stringify({
+            'type': "answer", 'answer': answer
+        })
+    }, memberId)
+}
+
+let addAnswer = async (answer) => {
+    if (!peerConnection.currentRemoteDescription) {
+        peerConnection.setRemoteDescription(answer)
+    }
 }
 
 init()
